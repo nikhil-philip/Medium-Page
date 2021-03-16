e3c4f08248fe446869b2e9da18ede0325afc5055
nikhil-philip <sabernick97@gmail.com>
2021-03-16 17:58:15 +0530

    Added top panel and Hamburger Menu

--- js/medium.js
+++ js/medium.js
@@ -1,21 +1,25 @@
 function ExpandOrCollapse(){
+  var panel = document.getElementById("panel");
   console.log("clicked")
  var menu = document.getElementById("menu");
  var status = menu.getAttribute("aria-expanded");
  if(status == "false")
  {
    menu.setAttribute("aria-expanded", "true");
-
+   
  }
  else
  {
    menu.setAttribute("aria-expanded", "false");
  }
- var panel = document.getElementById("panel");
+
  menu.classList.toggle("active");
- if (panel.style.maxHeight) {
-  panel.style.maxHeight = null;
-} else {
-  panel.style.maxHeight = panel.scrollHeight + "px";
-} 
+ if (panel.style.maxHeight) 
+  {
+    panel.style.maxHeight = null;
+  } 
+else 
+  {
+    panel.style.maxHeight = panel.scrollHeight + "px";
+  } 
 }
